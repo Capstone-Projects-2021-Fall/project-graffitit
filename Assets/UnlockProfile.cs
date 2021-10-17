2699a21ab46a6182ab9363c62c479956dcf39c3b
petermarion <59925687+petermarion@users.noreply.github.com>
2021-10-17 17:45:03 -0400

    Editing Profile
    Added edit button which:
    - Enables editing of the bio
    - Activates changeProfilePic button
    - Gets replaced with Save button
    
    Added Save button which does the opposite of Edit button.
    Save button does not yet have backend functionality.

---
+++ Assets/UnlockProfile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockProfile : MonoBehaviour
+{
+	public InputField profileBio;
+	public Button editProfileButton, saveProfileButton, profilePicButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	//TODO:
+    	//	Pull User Data from SQL Database
+    	//	Set Username text
+    	//	Set Profile picture
+    	//	Set Bio text
+
+    	editProfileButton.onClick.AddListener(activateEditing);
+    	saveProfileButton.onClick.AddListener(saveProfile);
+    }
+
+    void activateEditing()
+    {
+    	editProfileButton.gameObject.SetActive(false);
+        profileBio.interactable = true;
+        profilePicButton.gameObject.SetActive(true);
+        saveProfileButton.gameObject.SetActive(true);
+    }
+    void saveProfile()
+    {
+    	saveProfileButton.gameObject.SetActive(false);
+        profileBio.interactable = false;
+        profilePicButton.gameObject.SetActive(false);
+    	editProfileButton.gameObject.SetActive(true);
+
+    	//ADD PUSHING UPDATES TO SQL DATABASE
+    }
+}
