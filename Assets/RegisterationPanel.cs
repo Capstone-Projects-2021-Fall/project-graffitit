04e251b65096fa183a00924e84dd6f669b5cda8c
Wayne-Z-Code-To-Great <60633000+Wayne-Z-Code-To-Great@users.noreply.github.com>
2021-10-29 12:42:11 -0400

    Tested the connection to our MySQL database, and it was successful.

--- Assets/RegisterationPanel.cs
+++ Assets/RegisterationPanel.cs
@@ -1,10 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using MySql.Data.MySqlClient;
+using UnityEngine.UI;
+
 public class RegisterationPanel : MonoBehaviour
 {
+    private TMPro.TMP_InputField email;
+
+    static string connStr = "server=graffitit-database-instance-1.cakajll39l1z.us-east-2.rds.amazonaws.com; database=graffitit-database; Username=admin; Password=GraffitIT12";
+    private void Start()
+    {
+        email = GameObject.Find("Email").GetComponent<TMPro.TMP_InputField>();
+        
+        try
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+        } catch(MySqlException ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
     public void loadLoginPage()
     {
         SceneManager.LoadScene("LoginMenuPage");
@@ -14,4 +35,6 @@ public class RegisterationPanel : MonoBehaviour
     {
         SceneManager.LoadScene("TemHomePage");
     }
+
+
 }
